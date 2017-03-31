38adf3790497f8ddbaf7333948fdf6eea20de2ea
David Haynes <dhaynes207@gmail.com>
2017-03-30 23:25:58 -0400

    DelegateesParam: Add bare minimum class and comments
    This commit adds the DelegateesParam parameter to the repo. The Param
    actually does nothing at the moment but I intend to change that.
    
    Additionally a test class is birthed alongside this parameter that
    checks the existence of the file. Important stuff.
    
    Signed-off-by: David Haynes <dhaynes3@gmu.edu>

---
+++ src/DelegateesParam.ts
@@ -0,0 +1,37 @@
+/*
+ * lib-ical
+ * Copyright (C) 2017 Mark Stenglein
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+import Parameter from "./Parameter";
+
+/**
+ * DelegateesParam Class (Chapter x.x.x)
+ *
+ * - Purpose: 
+ *
+ * - Format Definition:
+ *
+ * - Description:
+ *
+ * - Example:
+ *
+ *
+ * @since 0.1.0
+ * @author David Haynes <dhaynes3@gmu.edu>
+ */
+export default class DelegateesParam extends Parameter {
+
+}
